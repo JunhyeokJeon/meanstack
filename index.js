2339d771d0ed88455f6c13ad36957c25b068f2b5
unknown <inamorfatis@gmail.com>
2018-02-12 23:04:50 +0900

    test

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+// Mongo DB
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', () => {
+  console.log('DB Connected');
+});
+db.on('error', (err) => {
+  console.log('DB Error: ', err);
+});
+
+// ejs 뷰 엔진
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-app.get('/hello', (req, res) => {
-  res.render('hello', {name: req.query.nameQuery})
+app.get('/main', (req, res) => {
+  res.render('main');
 });
 
-app.get('/hello/:nameParam', (req, res) => {
-  res.render('hello', {name: req.params.nameParam})
-});
 
+
+// app.get('/hello', (req, res) => {
+//   res.render('hello', {name: req.query.nameQuery})
+// });
+// app.get('/hello/:nameParam', (req, res) => {
+//   res.render('hello', {name: req.params.nameParam})
+// });
 
 
 app.listen(4004, () => {
